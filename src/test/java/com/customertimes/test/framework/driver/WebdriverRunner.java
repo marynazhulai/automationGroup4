2c9028b0bb783390868fe06a6b6fee0d729679ee
marynazhulai <marynazhulai@gmail.com>
2021-06-29 17:53:15 +0300

    Homework8

--- src/test/java/com/customertimes/test/framework/driver/WebdriverRunner.java
+++ src/test/java/com/customertimes/test/framework/driver/WebdriverRunner.java
@@ -39,7 +39,7 @@ public class WebdriverRunner {
                             capabilities.setCapability("browserName", "chrome");
                             capabilities.setCapability("browserVersion", "86.0");
                             capabilities.setCapability("enableVNC", true);
-                            driver.set(new RemoteWebDriver(new URL(TestConfig.CONFIG.seleniumServerUrl()), DesiredCapabilities.chrome()));
+                            driver.set(new RemoteWebDriver(new URL(TestConfig.CONFIG.seleniumServerUrl()), capabilities));
                         } catch (MalformedURLException e) {
                             e.printStackTrace();
                         }
